4d50ef911218b8592b41aa6e35a5288d0b3cc38a
Mihai Preda <mihai.preda@ubc.ca>
2024-11-25 17:19:16 -0800

    deleted mysql file

--- modules/profile/manifests/mysql.pp
+++
@@ -1,8 +0,0 @@
-# mysql profile
-class profile::mysql {
-  class { 'mysql::server':
-    root_password           => 'SweetC0rn',
-    remove_default_accounts => true,
-    restart                 => true,
-  }
-}
