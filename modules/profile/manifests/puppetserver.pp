46aeaff5dfe7842485008b9e20f5b02cd40f2afc
Mihai Preda <mihai.preda@ubc.ca>
2024-11-25 17:16:51 -0800

    added sysconfig file

--- modules/profile/manifests/puppetserver.pp
+++ modules/profile/manifests/puppetserver.pp
@@ -1,5 +1,14 @@
 # puppet server profile
 class profile::puppetserver {
+  package { 'puppetserver':
+    ensure => 'installed',
+  }
+  file { 'sysconfig-puppetserver':
+    ensure  => 'file',
+    path    => '/etc/sysconfig/puppetserver',
+    source  => 'puppet_course/files/sysconfig-puppetserver',
+    require => Package['puppetserver'],
+  }
   service { 'puppetserver':
     ensure   => 'running',
     enable   => true,
